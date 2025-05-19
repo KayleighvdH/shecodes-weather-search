e57bb9ff0f1d3f986e0e224a92f2d95e052ff045
Kayleigh van der Heijden <146675249+KayleighvdH@users.noreply.github.com>
2025-05-19 15:19:53 +0200

    Add basic search engine (no api)

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+function searchCity(event) {
+	event.preventDefault();
+	let searchInput = document.querySelector("#search-form-input");
+
+	let cityElement = document.querySelector("#weather-city");
+	cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchCity);
