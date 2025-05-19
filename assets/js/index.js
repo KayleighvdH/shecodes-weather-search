0a10b38ded190ab946ef4eb35d3f4efea6d5059b
Kayleigh van der Heijden <146675249+KayleighvdH@users.noreply.github.com>
2025-05-19 16:24:56 +0200

    Injected more weather data

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,42 @@
 function refreshWeather(response) {
+	let cityElement = document.querySelector("#weather-city");
 	let temperatureElement = document.querySelector("#weather-temperature-value");
 	let temperature = response.data.temperature.current;
+	let timeElement = document.querySelector("#weather-time");
+	let date = new Date(response.data.time * 1000);
+	let descriptionElement = document.querySelector("#weather-description");
+	let humidityElement = document.querySelector("#weather-humidity");
+	let windElement = document.querySelector("#weather-wind");
+	let windSpeed = response.data.wind.speed;
 
-	let cityElement = document.querySelector("#weather-city");
 	cityElement.innerHTML = response.data.city;
 
 	temperatureElement.innerHTML = Math.round(temperature);
+	timeElement.innerHTML = formatDate(date);
+	descriptionElement.innerHTML = response.data.condition.description;
+	humidityElement.innerHTML = response.data.temperature.humidity;
+	windElement.innerHTML = Math.round(windSpeed);
+}
+
+function formatDate(date) {
+	let days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	let day = days[date.getDay()];
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+
+	return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
